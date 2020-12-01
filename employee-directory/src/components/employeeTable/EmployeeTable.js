8a63b467bd169e5081a4c91cc6efba88eb1042d9
naywilkins512 <nathanwilkins01@gmail.com>
2020-12-01 12:31:36 -0600

    adds header and centers search bar

--- employee-directory/src/components/employeeTable/EmployeeTable.js
+++ employee-directory/src/components/employeeTable/EmployeeTable.js
@@ -7,7 +7,7 @@ function EmployeeTable() {
 
     const [employeeState, setEmployeeState] = useState(employees);
     const [searchState, setSearchState] = useState("");
-   
+
 
 
     // sorting function
@@ -51,10 +51,34 @@ function EmployeeTable() {
             <table className="table table-dark">
                 <thead>
                     <tr>
-                        <th scope="col">#</th>
-                        <th scope="col" onClick={() => sortBy('name')}>Name</th>
-                        <th scope="col">Occupation</th>
-                        <th scope="col">Location</th>
+                        <th scope="col">#
+                        <i onClick={() => sortBy('id')}>
+                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-down-short" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4z" />
+                                </svg>
+                            </i>
+                        </th>
+                        <th scope="col">Name
+                        <i onClick={() => sortBy('name')}>
+                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-down-short" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4z" />
+                                </svg>
+                            </i>
+                        </th>
+                        <th scope="col">Occupation
+                        <i onClick={() => sortBy('occupation')}>
+                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-down-short" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4z" />
+                                </svg>
+                            </i>
+                        </th>
+                        <th scope="col">Location
+                        <i onClick={() => sortBy('location')}>
+                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-down-short" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5A.5.5 0 0 1 8 4z" />
+                                </svg>
+                            </i>
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
--- employee-directory/src/components/nav/Nav.js
+++ employee-directory/src/components/nav/Nav.js
@@ -4,13 +4,15 @@ import React from 'react';
 function Nav(props) {
 
     return (
-<nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <>
+        <h1>Employee Directory</h1>
+<nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-center">
     <form className="form-inline my-2 my-lg-0">
         <input value={props.searchState} onChange={props.onChange} className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
         </input>
     </form>
 </nav>
-
+</>
     )
 };
 
